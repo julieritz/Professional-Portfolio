904374e002dc950d534f7cdcf8db67672a3ef9ab
julieritz <julie.ritz@gmail.com>
2020-09-02 10:47:14 -0400

    Updates

---
+++ precache-manifest.0ac54f2e97905d40e86985f331e624b2.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caf17b185030151aaaf6c7ac7b920f02",
+    "url": "/Professional-Portfolio/index.html"
+  },
+  {
+    "revision": "326b221a8d7732e8de9a",
+    "url": "/Professional-Portfolio/static/css/main.d0366851.chunk.css"
+  },
+  {
+    "revision": "ea81153de5cf7448f33d",
+    "url": "/Professional-Portfolio/static/js/2.d08c3573.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Professional-Portfolio/static/js/2.d08c3573.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "326b221a8d7732e8de9a",
+    "url": "/Professional-Portfolio/static/js/main.e77f4fba.chunk.js"
+  },
+  {
+    "revision": "1d76b48cb913433ef98c",
+    "url": "/Professional-Portfolio/static/js/runtime-main.303b969e.js"
+  },
+  {
+    "revision": "9bb831c4f4e62f58030cfd909dce077e",
+    "url": "/Professional-Portfolio/static/media/Budget-Tracker.9bb831c4.png"
+  },
+  {
+    "revision": "830a845ab9e2040da906aab71405609c",
+    "url": "/Professional-Portfolio/static/media/Code-Quiz.830a845a.png"
+  },
+  {
+    "revision": "2adaa64cdf273f475a0b8d66517053af",
+    "url": "/Professional-Portfolio/static/media/Date-Night-Helper.2adaa64c.png"
+  },
+  {
+    "revision": "707c454f988a320573ac8465bc866555",
+    "url": "/Professional-Portfolio/static/media/Eat-Da-Burger.707c454f.png"
+  },
+  {
+    "revision": "dda419fa178a3f0c03184f665a19bf66",
+    "url": "/Professional-Portfolio/static/media/Employee-Directory.dda419fa.png"
+  },
+  {
+    "revision": "2444ee1a12af3a9c027f176b44be5fcb",
+    "url": "/Professional-Portfolio/static/media/Employee-Tracker.2444ee1a.png"
+  },
+  {
+    "revision": "7a68e5499d46b7309595fc7a9df2570b",
+    "url": "/Professional-Portfolio/static/media/Fitness-Tracker.7a68e549.png"
+  },
+  {
+    "revision": "858fb26cfc7356161a02f4184b505015",
+    "url": "/Professional-Portfolio/static/media/Note-Taker.858fb26c.png"
+  },
+  {
+    "revision": "4effbd72645e48a72619d0be55482f52",
+    "url": "/Professional-Portfolio/static/media/Represent-Me.4effbd72.png"
+  },
+  {
+    "revision": "51f91e81cce597ad29f69d65de1045bd",
+    "url": "/Professional-Portfolio/static/media/Template-Engine.51f91e81.png"
+  },
+  {
+    "revision": "b468a2dc21f38aa6efab7bbae16c4469",
+    "url": "/Professional-Portfolio/static/media/Tiny-Learn.b468a2dc.png"
+  },
+  {
+    "revision": "8e14c63931dc07f65b36248f1dfb3084",
+    "url": "/Professional-Portfolio/static/media/Work-Day-Scheduler.8e14c639.png"
+  },
+  {
+    "revision": "daf3740893b4a25062d41e9ae6e3ad3a",
+    "url": "/Professional-Portfolio/static/media/circle-cropped.daf37408.png"
+  },
+  {
+    "revision": "058035fa42f9aa78759ac1bd68092c5b",
+    "url": "/Professional-Portfolio/static/media/flowers-left.058035fa.jpg"
+  },
+  {
+    "revision": "4618a560dede735553d6b1edf2bd6936",
+    "url": "/Professional-Portfolio/static/media/flowers-right.4618a560.jpg"
+  },
+  {
+    "revision": "c9dfc8ed50e7887018b0c3cef4d37144",
+    "url": "/Professional-Portfolio/static/media/long-flowers.c9dfc8ed.jpg"
+  }
+]);
